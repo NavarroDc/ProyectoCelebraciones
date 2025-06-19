ef7f1397ddc792448964f4b4dbec751e4ac1c159
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-06-18 19:35:15 -0600

    clase gestor y celebracion creadas

---
+++ ED DavidNavarroC-Tarea1/src/ed/davidnavarroc/tarea1/Celebracion.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed.davidnavarroc.tarea1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author angel
+ */
+public class Celebracion {
+    
+    private int id;
+    private Date fecha;
+    private String descripcion;
+    private String pais;
+
+    public Celebracion(int id, Date fecha, String descripcion, String pais) {
+        this.id = id;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.pais = pais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+   //Acá se borró set ID porque no es editable
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
---
+++ ED DavidNavarroC-Tarea1/src/ed/davidnavarroc/tarea1/Gestor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed.davidnavarroc.tarea1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author angel
+ */
+public class Gestor {
+    
+    private ArrayList<Celebracion> celebraciones = new ArrayList<>();
+    
+}
